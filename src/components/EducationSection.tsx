d2ca212d5191eb0a1f7ee735e8f649c802e1a4f9
Dev <devreddy4444@gmail.com>
2025-07-28 05:17:28 +0530

    feat: add Education section

---
+++ src/components/EducationSection.tsx
@@ -0,0 +1,46 @@
+import {
+  MultiAccordionSection,
+  SubItem,
+} from "@/components/custom/MultiAccordionSection";
+
+const educationSectionItems: SubItem[] = [
+  {
+    title: (
+      // <div className="flex flex-row justify-between w-full items-center">
+      //   <div className="text-xl">Auror | Indie Hacker</div>
+      //   <div className="text-sm md:text-lg">Jul &apos;25 - Present</div>
+      // </div>
+      <div className="w-full">
+        <div className="hidden md:block">
+          <div className="flex flex-row justify-between w-full items-center">
+            <div className="text-xl">B. Tech in CSE (8.22 CGPA)</div>
+            <div className="text-sm md:text-lg">
+              Nov &apos;21 - Jun &apos;25
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden flex flex-col">
+          <div className="flex flex-row justify-between w-full items-center">
+            <div className="text-xl">B. Tech</div>
+            <div className="text-sm md:text-lg">
+              Nov &apos;21 - Jun &apos;25
+            </div>
+          </div>
+          <div className="text-lg font-bold">CSE (8.22 CGPA)</div>
+        </div>
+      </div>
+    ),
+    content: ["University: Rajiv Gandhi Proudyogiki Vishwavidyalaya (R.G.P.V)"],
+  },
+];
+
+export function EducationSection() {
+  return (
+    <div className="w-full mx-auto mt-8 ">
+      <MultiAccordionSection
+        sectionTitle="Education"
+        items={educationSectionItems}
+      />
+    </div>
+  );
+}
