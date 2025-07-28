a6a1de82a66b686fb8593ad96e339b7045082211
Dev <devreddy4444@gmail.com>
2025-07-28 20:21:17 +0530

    feat: add experience content

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -31,9 +31,9 @@ const experienceSectionItems: SubItem[] = [
       </div>
     ),
     content: [
-      "Built MVP for an AI-powered mock interview platform",
-      "Integrated real-time feedback, scoring, and analytics",
-      "Built with Next.js, PostgreSQL, Supabase, Python, and LLMs",
+      "• Currently building the MVP for an AI-powered mock interview platform integrating real-time voice agents",
+      "• Users can choose from featured curated interviews of big MNCs to Startups",
+      "• Users can also create their custom interview to practice for any job interview",
     ],
   },
   {
@@ -50,7 +50,7 @@ const experienceSectionItems: SubItem[] = [
         <div className="hidden md:block">
           <div className="flex flex-row justify-between w-full items-center">
             <div className="text-xl">
-              <span className="font-bold">Swayatt</span>| Full Stack Developer
+              <span className="font-bold">Swayatt</span> | Full Stack Developer
               Intern
             </div>
             <div className="text-sm md:text-lg">
@@ -76,9 +76,12 @@ const experienceSectionItems: SubItem[] = [
       >
         Visit Website <ExternalLink className="w-4 h-4" />
       </Link>,
-      "Created internal admin dashboards using React",
-      "Reduced API response time by optimizing backend routes",
-      "Worked closely with product team to ship features quickly",
+      "• Led the development of HMI (Human-Machine Interface) monitoring system for BrahMos Aerospace that processes 10+ machine metrics/second from industrial sensors using InfluxDB and MERN stack with Socket.IO, reducing data latency to <200ms while optimizing server CPU load by over 90%.",
+      "• Building the company’s new website using Next.js 14 and TypeScript, implementing SSR/SSG rendering and MongoDB-integrated forms, achieving 90+ PageSpeed scores in initial tests.",
+      "• Built an email notification system for CMMS (Computerized Maintenance Management System) processing 1K+ daily alerts across 7 modules and opt-in controls reducing notification overload by 25%.",
+      "• Engineered CMMS-HMI integration using MongoDB change streams and RBAC-controlled Socket.IO rooms, achieving <200ms cross-system synchronization with 100% data consistency.",
+      "• Architected downtime resolution system that auto-triggers HMI-blocking pages for outages >1 hour, enforcing 90% incident documentation compliance and reducing unresolved downtime by 80%.",
+      "• Designed and shipped end-to-end request-to-work-order workflow with state transition modeling and RBAC views to handle 200+ monthly tickets, cutting closure time by 30% and securing 2 new client acquisitions.",
     ],
   },
 ];
